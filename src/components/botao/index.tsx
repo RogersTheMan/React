aa3277d566457e8f449e2a8cfb11916844519356
Rodrigo Pretel <pretrod971@gmail.com>
2023-08-08 16:03:26 -0300

    aula 5 - state

--- src/components/botao/index.tsx
+++ src/components/botao/index.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import style from "./Botao.module.scss"
 
-class Botao extends React.Component <{ texto : string}>{
-  render(){
+class Botao extends React.Component <{ texto : string, type?: "button" | "submit" | "reset" | undefined}>{
+  render(){ 
+    const { type	= "button" } = this.props
     return (
-      <button className={style.botao}>{this.props.texto}</button>
+      <button type={type} className={style.botao}>{this.props.texto}</button>
     )
   }
 }
--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,14 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Formulario from '../components/formulario';
 import Lista from '../components/lista';
 import Style from "./App.module.scss"
 import Cronometro from '../components/Cronometro';
+import { ITarefa } from '../types/tarefa';
 
 function App() {
+  const [tarefas, setTarefas] = useState<ITarefa[]>([]);
   return (
     <div className={Style.AppStyle}>
-      <Formulario />
-      <Lista />
+      <Formulario setTarefas={setTarefas}/>
+      <Lista tarefas={tarefas} />
       <Cronometro />
     </div>
   );
