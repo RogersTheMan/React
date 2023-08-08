a644220e407b1a8e402865f07314503d4c8bf769
Rodrigo Pretel <pretrod971@gmail.com>
2023-08-08 11:05:24 -0300

    aula 4

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import Formulario from '../components/formulario';
 import Lista from '../components/lista';
 import Style from "./App.module.scss"
+import Cronometro from '../components/Cronometro';
 
 function App() {
   return (
     <div className={Style.AppStyle}>
       <Formulario />
       <Lista />
+      <Cronometro />
     </div>
   );
 }
